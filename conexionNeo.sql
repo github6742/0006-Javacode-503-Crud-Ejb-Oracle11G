a6dc56e7b7b21cbf7b8eb2d0acee018b96538eb1
segarod <srodriguezlab@yahoo.com>
2021-05-30 01:45:36 -0300

    Commit video 2

--- conexionNeo.sql
+++ conexionNeo.sql
@@ -9,3 +9,19 @@ telefono VARCHAR2(15) not null
 CREATE SEQUENCE incremento_id_alumno
 INCREMENT BY 1
 START WITH 1;
+
+CREATE TRIGGER TRG_ID_ALUMNO
+BEFORE INSERT ON ALUMNO
+FOR EACH ROW
+BEGIN
+SELECT INCREMENTO_ID_ALUMNO.NEXTVAL INTO :NEW.IDALUMNO FROM DUAL;
+END;
+
+
+select * from alumno
+
+insert into alumno(nombres, apellidos, direccion, telefono)values('JOSE ERNESTO','ZURITA NUÑEZ','AV. LA COLINA 2, CASA#12','7845-9623');
+insert into alumno(nombres, apellidos, direccion, telefono)values('DAVID ANTONIO','LOPEZ CRUZ','CALLE INDEPENDENCIA, CASA#11','7158-6242');
+insert into alumno(nombres, apellidos, direccion, telefono)values('BLANCA MARGARITA','CARIAS RUIZ','AV. LAS MAGNOLIAS SUR, CASA#8','7348-958');
+insert into alumno(nombres, apellidos, direccion, telefono)values('STAYCU CECILIA','MENDEZ SUEZ','COL. EL ZONTLE, CASA#2','7956-8542');
+insert into alumno(nombres, apellidos, direccion, telefono)values('REBECA ABIGAIL','SERMEÑO AGUIRRE','RES. BOSQUE DE LA PAZ, POL A, CASA#29','7412-5836');
